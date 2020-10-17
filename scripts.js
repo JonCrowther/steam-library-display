7822e548e44953f84b79ab30466413626dc55484
JonCrowther <9119124+JonCrowther@users.noreply.github.com>
2020-10-17 12:02:02 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+/*
 const app = document.getElementById('root')
 
 const logo = document.createElement('img')
@@ -8,6 +9,8 @@ container.setAttribute('class', 'container')
 
 app.appendChild(logo)
 app.appendChild(container)
+*/
+
 /*
 var request = new XMLHttpRequest()
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
